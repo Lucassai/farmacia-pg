624ce2cb577593a42696893d4f6c1f83971ce52e
Lucas Oliveira <lucasvmo42@gmail.com>
2025-01-27 11:33:42 -0300

    componentes navbar,footer e home criados

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <>
+      <div
+        className="w-full flex justify-center py-4
+            			   bg-pink-400 text-white"
+      >
+        <div className="container flex justify-between text-xl">
+          Farmacia PG
+          <div className="flex gap-4">Categoria Produto</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,38 +1,22 @@
 function Home() {
   return (
     <>
-      <div
-        style={{
-          width: "100vw",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div>
-          <div
-            style={{
-              width: "80vw",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <h2>Seja Bem Vinde!</h2>
-            <p>Expresse aqui seus pensamentos e opniões</p>
+      <div className="bg-pink-300 flex justify-center">
+        <div className="container grid grid-cols-2 text-white">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-5xl font-bold">Farmácia PG</h2>
+            <p className="text-xl">Aqui você encontra de tudo</p>
+
+            <div className="flex justify-around gap-4">
+              <div className="flex justify-around gap-4"></div>
+            </div>
           </div>
 
-          <div
-            style={{
-              width: "80vw",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
+          <div className="flex justify-center ">
             <img
-              src="https://i.imgur.com/VpwApCU.png"
-              alt="Imagem da Página Home"
-              width="400px"
+              src="https://png.pngtree.com/thumb_back/fw800/background/20230929/pngtree-vibrant-array-of-medication-stunning-3d-visualization-image_13526019.png"
+              alt="Imagem Farmacia"
+              className="w-full"
             />
           </div>
         </div>
